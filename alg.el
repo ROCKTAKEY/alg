5610caaa5b1d11f44cf9c1a884fa6646afbf8ee6
ROCKTAKEY <rocktakey@gmail.com>
2022-03-30 23:07:47 +0900

    docs: Update version.

--- alg.el
+++ alg.el
@@ -5,7 +5,7 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: tools
 
-;; Version: 0.2.4
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.7"))
 ;; URL: https://github.com/ROCKTAKEY/alg
 
